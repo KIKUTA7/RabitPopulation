e6d85228f3c2d1a4784b7ffa406b844d6b0ed56e
Beka Kikutadze <kikutadze.beka@kiu.edu.ge>
2021-10-08 09:55:38 +0200

    Changes by Online Editor

--- src/fop/w2rabbits/RabbitPopulation.java
+++ src/fop/w2rabbits/RabbitPopulation.java
@@ -3,6 +3,19 @@ package fop.w2rabbits;
 public class RabbitPopulation extends MiniJava {
 
     public static void main(String[] args){
-	// TODO
+int x=0,y=0,z=1;
+int n=readInt();
+int i=1;
+while (i<=n)
+{
+    int sum=x+y+z;
+    int sec=z;
+    int fir=y;
+    x=fir;
+    y=sec;
+    z=sum;
+    i++;
+}
+write(z);
     }
 }
